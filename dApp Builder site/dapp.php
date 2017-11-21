ddf76cb4696faa44cea9aaaf04c3f600bfc5b3be
Sergey Lobanov <slobanov@solovat.net>
2017-11-21 11:52:56 +0300

    Voting dApp fix

---
+++ dApp Builder site/dapp.php
@@ -0,0 +1,29 @@
+<?php
+
+if (empty($_GET['id'])) exit;
+
+require_once('classes/Helper.php');
+session_start();
+
+$currentUser = Helper::getCurrentUser();
+
+$id = $_GET['id'];
+
+$db = db::getInstance();
+$query = $db->prepare('SELECT * FROM `dapps` WHERE `deployed` = 1 AND `id` = :id');
+$query->bindParam(':id', $id);
+$query->execute();
+$dapp = $query->fetchObject('Dapp');
+
+if (!$dapp) exit;
+
+$type = $dapp->getDappType();
+$name = $dapp->getName();
+$eth_account = $dapp->getEthAccount();
+$interface = $dapp->getInterface();
+
+switch ($type) {
+	case 'voting':
+		require_once 'dapps/voting.php';
+		break;
+}
