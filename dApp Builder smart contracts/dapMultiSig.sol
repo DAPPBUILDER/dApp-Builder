038d7a354f5b3d8a1080521153031bf6e7f20819
Alexander <fivehundreth@gmail.com>
2018-02-19 15:45:32 +0300

    added executeTxn method, modifiers

--- dApp Builder smart contracts/dapMultiSig.sol
+++ dApp Builder smart contracts/dapMultiSig.sol
@@ -23,12 +23,28 @@ contract ibaMultisig {
         uint appovalsreq;
         bool initialized;
     }
-
+    
+    /*
+    * Modifiers
+    */
+    modifier onlyOwner ( address creator, uint walletId ) {
+        bool found;
+        for (uint i = 0;i<wallets[creator][walletId].owners.length;i++){
+            if (wallets[creator][walletId].owners[i] == msg.sender){
+                found = true;
+            }
+        }
+        if (found){
+            _;
+        }
+    }
+    
     /*
     * Events
     */
     event WalletCreated(uint id);
     event TxnSumbitted(uint id);
+    event TxnConfirmed(uint id);
 
     /*
     * Storage
@@ -43,12 +59,28 @@ contract ibaMultisig {
     }
 
     /*
+    * Getters
+    */
+    function getWalletsNum(address creator) external view returns (uint num){
+        return wallets[creator].length;
+    }
+
+    function getOwners(address creator, uint id) external view returns (address[]){
+        return wallets[creator][id].owners;
+    }
+    
+    function getTxnNum(address creator, uint id) external view returns (uint){
+        require(wallets[creator][id].initialized == true);
+        return wallets[creator][id].transactions.length;
+    }
+    
+    /*
     * Methods
     */
     function createWallet(uint approvals, address[] owners) payable external returns (bool){
 
         /*check if approvals num equals or greater than given owners num*/
-        assert(approvals <= owners.length);
+        require(approvals <= owners.length);
 
         /*instantiate new wallet*/
         uint currentLen = wallets[msg.sender].length++;
@@ -62,15 +94,7 @@ contract ibaMultisig {
         return true;
     }
 
-    function getWalletsNum(address creator) public view returns (uint num){
-        return wallets[creator].length;
-    }
-
-    function getOwners(address creator, uint id) public view returns (address[]){
-        return wallets[creator][id].owners;
-    }
-
-    function submitTransaction(address creator, address destination, uint walletId, uint value, bytes data) external returns (bool){
+    function submitTransaction(address creator, address destination, uint walletId, uint value, bytes data) onlyOwner (creator,walletId) external returns (bool) {
         if (wallets[creator][walletId].initialized == true){
             uint newTxId = wallets[creator][walletId].transactions.length++;
             wallets[creator][walletId].transactions[newTxId].destination = destination;
@@ -81,43 +105,42 @@ contract ibaMultisig {
         }
     }
 
-    function confirmTransaction(address creator, uint walletId, uint txId) external returns (bool){
-        bool found;
-        for (uint i = 0;i<wallets[creator][walletId].owners.length;i++){
-            if (wallets[creator][walletId].owners[i] == msg.sender){
-                found = true;
-            }
-        }
-        if (found){
-            Wallet storage wallet = wallets[creator][walletId];
-            Transaction storage txn = wallet.transactions[txId];
+    function confirmTransaction(address creator, uint walletId, uint txId) onlyOwner(creator, walletId) external returns (bool){
+        Wallet storage wallet = wallets[creator][walletId];
+        Transaction storage txn = wallet.transactions[txId];
 
-            txn.confirmed.push(msg.sender);
-
-            /* check whether transaction has reached required amount of confirmations */
-            if (txn.confirmed.length == wallet.appovalsreq){
-
-                /* check whether wallet has sufficient balance to send this transaction */
-                if (wallet.balance >= txn.value){
-
-                    /* change transaction's status to executed */
-                    txn.executed == true;
-
-                    /* change wallet's balance */
-                    wallet.balance = wallet.balance - txn.value;
-
-                    /* send transaction */
-                    address dest = txn.destination;
-                    uint val = txn.value;
-                    bytes memory dat = txn.data;
-                    assert(this.call(dest,val,dat));
-                    return true;
-                }
-            }
-            return true;
-        } else {
-            return false;
-        }
+        txn.confirmed.push(msg.sender);
+        
+        TxnConfirmed(txId);
+        
+        return true;
     }
+    
+    function executeTxn(address creator, uint walletId, uint txId) onlyOwner(creator, walletId) external returns (bool){
+        Wallet storage wallet = wallets[creator][walletId];
+        require (wallet.initialized == true);
+        
+        Transaction storage txn = wallet.transactions[txId];
+        
+        /* check whether transaction has reached required amount of confirmations */
+        require(txn.confirmed.length >= wallet.appovalsreq);
+        
+        /* check whether wallet has sufficient balance to send this transaction */
+        require(wallet.balance >= txn.value);
+        
+        /* send transaction */
+        address dest = txn.destination;
+        uint val = txn.value;
+        bytes memory dat = txn.data;
+        assert(dest.call.value(val)(dat));
+            
+        /* change transaction's status to executed */
+        txn.executed == true;
 
+        /* change wallet's balance */
+        wallet.balance = wallet.balance - txn.value;
+
+        return true;
+        
+    }
 }
