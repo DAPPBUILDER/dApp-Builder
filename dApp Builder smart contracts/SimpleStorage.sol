16ca254a003ecfe5c3074f5ce6c27be3fe1f04d4
Ankit Jangir <45584740+ankittjangir1690@users.noreply.github.com>
2020-10-01 13:32:51 +0530

    Add files via upload

---
+++ dApp Builder smart contracts/SimpleStorage.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.5.9; 
+// Imagine a big integer that the whole world could share
+contract SimpleStorage {
+    uint storedData;
+
+    function set(uint x) public {
+        storedData = x;
+    }
+
+    function get() view public returns (uint) {
+        return storedData;
+    }
+    
+    function increment (uint n) public {
+        storedData = storedData + n;
+        return;
+    }
+    
+    function decrement (uint n) public {
+        storedData = storedData - n;
+        return;
+    }
+    
+}
+
+ 
+
+
